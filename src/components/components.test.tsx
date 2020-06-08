0f67c8c1f966b53027f9243cbc5f7b80c77c8456
Jakob J. Kolb <kolb@hardfork.io>
2020-06-08 16:44:55 +0200

    move function tests to separate file

--- src/components/components.test.tsx
+++ src/components/components.test.tsx
@@ -1,22 +1,10 @@
 import React from 'react'
-import { calculateWinner } from './Game'
 import { shallow, configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import Square from './square'
 import Board from './board'
 
 configure({ adapter: new Adapter() })
-
-describe('calculateWinner', () => {
-  test('should return winner if she has three in line', () => {
-    expect(
-      calculateWinner(['X', 'X', 'X', null, 'O', 'O', null, null, null, null]),
-    ).toEqual('X')
-  })
-  test('should return null, if noone has three in line', () => {
-    calculateWinner(['X', 'X', 'O', null, 'O', 'O', null, 'X', null, null])
-  })
-})
 
 describe('Square', () => {
   test('should render correctly', () => {

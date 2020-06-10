f99ad5cf850ece8e8c675d867b9c277b1e086b91
Jakob J. Kolb <kolb@hardfork.io>
2020-06-10 19:21:37 +0200

    update jumpReducer to reset history

--- src/helpers/helpers.test.ts
+++ src/helpers/helpers.test.ts
@@ -1,4 +1,7 @@
-import { calculateWinner } from '.'
+import { calculateWinner, jumpReducer } from '.'
+import { GameState } from 'components/Game'
+import { PLAYERS } from 'types'
+import R from 'ramda'
 
 describe('calculateWinner', () => {
   test('should return winner if she has three in line', () =>
@@ -9,4 +12,44 @@ describe('calculateWinner', () => {
     expect(
       calculateWinner(['X', 'X', 'O', null, 'O', 'O', null, 'X', null, null]),
     ).toEqual(null))
+})
+
+describe('jumpReducer', () => {
+  const state: GameState = {
+    history: [
+      {
+        squares: [null, null, null, null, null, null, null, null, null],
+        xIsNext: true,
+      },
+      {
+        squares: [PLAYERS.X, null, null, null, null, null, null, null, null],
+        xIsNext: false,
+      },
+      {
+        squares: [
+          PLAYERS.X,
+          PLAYERS.O,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+          null,
+        ],
+        xIsNext: true,
+      },
+    ],
+    stepNumber: 2,
+  }
+  test('should return new state with stepNumber according to action', () => {
+    expect(jumpReducer(state, { type: 'JUMP', step: 1 })).toMatchObject({
+      stepNumber: 1,
+    })
+  })
+  test('should return state that does not contain history above what is indicated by step number', () => {
+    expect(
+      R.prop('history')(jumpReducer(state, { type: 'JUMP', step: 1 })),
+    ).toHaveLength(2)
+  })
 })

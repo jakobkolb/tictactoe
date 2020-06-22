57aea85d0e13653d41f94cf339db4b7cc1aaf0ad
Jakob J. Kolb <kolb@hardfork.io>
2020-06-22 10:56:42 +0200

    Cleanup Board component.

--- src/components/Game/Board/index.tsx
+++ src/components/Game/Board/index.tsx
@@ -2,26 +2,22 @@ import React from 'react'
 import { Square } from './Square/Square'
 
 export const GameBoard: React.SFC = () => {
-  const renderSquare = (i: number) => {
-    return <Square location={i} />
-  }
-
   return (
     <div>
       <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
+        <Square location={0} />
+        <Square location={1} />
+        <Square location={2} />
       </div>
       <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
+        <Square location={3} />
+        <Square location={4} />
+        <Square location={5} />
       </div>
       <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
+        <Square location={6} />
+        <Square location={7} />
+        <Square location={8} />
       </div>
     </div>
   )
